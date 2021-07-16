5c11604cf9aaabc5b11a65647d749cd6496b820e
tetra <me@tetra.cool>
2021-07-16 01:49:05 -0400

    now uses nameplate opacity setting

---
+++ ProPlates/Components/OpacityListener.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace ProPlates.Components
+{
+    public class OpacityListener : MonoBehaviour
+    {
+        public OpacityListener(IntPtr ptr) : base(ptr) {}
+        public ImageThreeSlice reference;
+        public ImageThreeSlice target;
+        
+        private void Update()
+        {
+            if (target.color == reference.color) return;
+            target.color = reference.color;
+        }
+    }
+}
