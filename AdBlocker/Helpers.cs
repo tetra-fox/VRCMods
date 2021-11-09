bc530126d7974b4063fd7f0dc5eb576d089f4f65
Tetra <me@tetra.cool>
2021-11-09 12:39:28 -0500

    adblocker actually works now

--- AdBlocker/Helpers.cs
+++ AdBlocker/Helpers.cs
@@ -1,14 +1,28 @@
-﻿using System.Linq;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace AdBlocker
 {
-    public static class Helpers
+    internal static class Helpers
     {
-        public static GameObject FindObject(this GameObject parent, string name)
+        // very bad and inefficient do not use
+        // or do i don't really care
+        internal static GameObject FindInactive(string path)
         {
-            Transform[] tfs = parent.GetComponentsInChildren<Transform>(true);
-            return (from t in tfs where t.name == name select t.gameObject).FirstOrDefault();
+            string[] hierarchy = path.Split('/');
+            GameObject currentObject = GameObject.Find(hierarchy[0]);
+            string targetObjectName = hierarchy[hierarchy.Length - 1];
+            int cursor = 0;
+            
+            // also check if cursor is at end of path (in case gameobject names are reused in the hierarchy)
+            while (currentObject.name != targetObjectName && cursor != hierarchy.Length)
+            {
+                GameObject nextObject = currentObject.transform.Find(hierarchy[cursor + 1]).gameObject;
+                currentObject = nextObject;
+                cursor++;
+                // if nextObject is null, unity will just throw a NullReferenceException, stopping this loop
+            }
+            
+            return currentObject;
         }
     }
 }
