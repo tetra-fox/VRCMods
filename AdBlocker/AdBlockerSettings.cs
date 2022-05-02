68b239ee8e078d459a870e98b47ae020f80dd4da
Tetra <me@tetra.cool>
2022-05-02 05:42:17 -0400

    map incorrect pref names to fixed pref names

--- AdBlocker/AdBlockerSettings.cs
+++ AdBlocker/AdBlockerSettings.cs
@@ -1,5 +1,6 @@
 using MelonLoader;
 using System;
+using System.Collections.Generic;
 
 namespace AdBlocker;
 
@@ -13,6 +14,7 @@ internal static class Settings
     public static MelonPreferences_Entry<bool> RemoveVrcPlusTab;
     public static MelonPreferences_Entry<bool> RemoveVrcPlusPfp;
     public static MelonPreferences_Entry<bool> RemoveVrcPlusGetMoreFavorites;
+    public static MelonPreferences_Entry<bool> PrefsFixed;
 
     public static event Action OnConfigChanged;
 
@@ -29,9 +31,30 @@ internal static class Settings
         RemoveVrcPlusPfp = Prefs.CreateEntry(nameof(RemoveVrcPlusPfp), false, "Remove VRC+ PFP button");
         RemoveVrcPlusSupporter = Prefs.CreateEntry(nameof(RemoveVrcPlusSupporter), false, "Remove VRC+ supporter button");
         RemoveVrcPlusTab = Prefs.CreateEntry(nameof(RemoveVrcPlusTab), false, "Remove VRC+ tab");
+        PrefsFixed = Prefs.CreateEntry(nameof(PrefsFixed), false, is_hidden: true);
 
+        if (!PrefsFixed.Value) FixPrefs();
+        
         OnConfigChanged += () => Changed = true;
 
         foreach (MelonPreferences_Entry e in Prefs.Entries) e.OnValueChangedUntyped += () => OnConfigChanged?.Invoke();
+    }
+
+    private static void FixPrefs()
+    {
+        // This needed to be implemented because I'm really dumb and completely messed up the preference mappings :(
+        Dictionary<MelonPreferences_Entry<bool>, bool> fixedPrefMap = new()
+        {
+            {RemoveVrcPlusSupporter, RemoveVrcPlusTab.Value},
+            {RemoveVrcPlusGift, RemoveVrcPlusSupporter.Value},
+            {RemoveVrcPlusTab, RemoveVrcPlusPfp.Value},
+            {RemoveVrcPlusPfp, RemoveVrcPlusGift.Value}
+        };
+
+        foreach (KeyValuePair<MelonPreferences_Entry<bool>, bool> mapping in fixedPrefMap)
+            mapping.Key.Value = mapping.Value;
+        
+        PrefsFixed.Value = true;
+        Prefs.SaveToFile();
     }
 }
